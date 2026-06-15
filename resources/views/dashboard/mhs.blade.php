581ea6cf48f9a3b3ef144a46af4e2df6787ee96e
Muhammad Zaky Irly Alqifari <mhdzaky502@gmail.com>
2026-06-16 01:18:35 +0700

    Fixed Tampilan Mhs

--- resources/views/dashboard/mhs.blade.php
+++ resources/views/dashboard/mhs.blade.php
@@ -1,21 +1,18 @@
 <div class="col-md-12 grid-margin stretch-card">
     <div class="card">
         <div class="card-body">
-            <div class="d-flex flex-column flex-md-row align-items-start justify-content-between">
-                <div>
-                    <h4 class="card-title">Dashboard Mahasiswa</h4>
-                    <p class="card-description mb-0">Daftar pinjaman buku Anda.</p>
-                </div>
-                <div class="mt-3 mt-md-0">
-                    <h4 class="card-title">Daftar Pinjaman</h4>
-                </div>
+            
+            <div class="mb-4">
+                <h4 class="card-title">Dashboard Mahasiswa</h4>
+                <p class="card-description">Daftar pinjaman buku Anda.</p>
+                <h4 class="card-title mt-4">Daftar Pinjaman</h4>
             </div>
 
             @if(session('success'))
                 <div class="alert alert-success mt-3">{{ session('success') }}</div>
             @endif
 
-            <div class="table-responsive mt-4">
+            <div class="table-responsive mt-3">
                 <table class="table">
                     <thead>
                         <tr>
